eb1c0999b8115e829c59e2f0c43e3345570691f1
Bardyy <battar@uwo.ca>
2020-12-02 10:58:14 -0500

    Create, Update, and Delete features
    Users can now create, update, and delete their schedules.

--- se3316-lab5/src/app/backend/backend.component.ts
+++ se3316-lab5/src/app/backend/backend.component.ts
@@ -27,4 +27,35 @@ export class BackendComponent implements OnInit {
       })
   }
 
+  createSchedule() {
+    if (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(this.scheduleInput)) {
+      this.validator = false;
+      alert('ERROR: You have entered illegal characters.')
+
+    } else {
+      this.validator = true;
+      this.scheduleUpdater.createSchedule(this.scheduleInput)
+      alert('You have created a schedule with the name: ' + this.scheduleInput)
+      location.reload();
+    }
+  }
+
+  deleteSchedules() {
+    this.scheduleUpdater.deleteSchedule(this.ROOT_URL + '/api/courses/schedule')
+    alert('All schedules have been deleted')
+    location.reload();
+
+  }
+
+  updateSchedule() {
+
+    this.scheduleUpdater.modifySchedule(this.scheduleName, this.scheduleList)
+    this.scheduleSubscriber = this.scheduleUpdater.getScheduleUpdaterListener()
+      .subscribe(data => {
+        this.scheduleList = data
+      })
+    alert('The schedule has been updated')
+    location.reload();
+  }
+
 }
