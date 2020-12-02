3a76f151c11223898bedbd8bb76682e6a56ec82a
Bardyy <battar@uwo.ca>
2020-12-02 10:52:58 -0500

    Enabled subject search function
    Created the subject search function for the front end

--- se3316-lab5/src/app/app-routing.module.ts
+++ se3316-lab5/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BackendComponent } from './backend/backend.component';
 import { FrontendComponent } from './frontend/frontend.component';
--- se3316-lab5/src/app/frontend/frontend.component.ts
+++ se3316-lab5/src/app/frontend/frontend.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import { Router } from '@angular/router';
+import TimeTableData from '../TimeTableData.json';
 
 @Component({
   selector: 'app-frontend',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./frontend.component.css']
 })
 export class FrontendComponent implements OnInit {
+  @Output() isLogout = new EventEmitter<void>();
+  courseInfo: any = TimeTableData;
+  subject_entry = ''
+  component_entry = ''
+  array_1 = [] as any[];
+  isSignedIn = false;
 
-  constructor() { }
+
+  constructor(public firebaseService: FirebaseService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  subjectSearch() {
+    for (let i = 0; i < this.courseInfo.length; i++) {
+      if ((this.courseInfo[i].subject === this.subject_entry && this.courseInfo[i].catalog_nbr === this.component_entry)) {
+
+        this.array_1.push({
+          "course_info": this.courseInfo[i].course_info
+        });
+        console.log(this.array_1)
+      }
+
+
+    }
+    if (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(this.subject_entry) || (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(this.component_entry))) { alert('ERROR: You have entered illegal characters.') }
+  }
 }
