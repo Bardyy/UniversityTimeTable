fadefad92f761aef2c1c91211f4ae2d40dbdb3b5
Bardyy <battar@uwo.ca>
2020-12-02 10:56:50 -0500

    View a schedule function
    Users can now view available schedules

--- se3316-lab5/src/app/backend/backend.component.ts
+++ se3316-lab5/src/app/backend/backend.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ScheduleUpdater } from './backend.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-backend',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./backend.component.css']
 })
 export class BackendComponent implements OnInit {
+  private scheduleSubscriber: Subscription;
+  get_schedules: any;
+  scheduleInput: string;
+  validator = false;
+  scheduleName: string
+  scheduleList: any
+  readonly ROOT_URL = 'http://localhost:3000'
 
-  constructor() { }
+  constructor(private http: HttpClient, public scheduleUpdater: ScheduleUpdater) { }
 
   ngOnInit(): void {
+    this.scheduleUpdater.getSchedules()
+    this.scheduleSubscriber = this.scheduleUpdater.getScheduleUpdaterListener()
+      .subscribe(data => {
+        this.get_schedules = data
+      })
   }
 
 }
