537bdc92d929f9e022a1fa350c4fa42f69baae68
Bardyy <battar@uwo.ca>
2020-12-02 19:43:47 -0500

    course search functions.
    Subjects and courses can now be searched either by subject, subject and course, and subject,course, and component. Entries are also case insensitive

--- se3316-lab5/src/app/frontend/frontend.component.ts
+++ se3316-lab5/src/app/frontend/frontend.component.ts
@@ -14,17 +14,18 @@ export class FrontendComponent implements OnInit {
   subject_entry = ''
   component_entry = ''
   array_1 = [] as any[];
+
   isSignedIn = false;
+  empty = "";
 
 
   constructor(public firebaseService: FirebaseService, private router: Router) { }
 
   ngOnInit(): void {
   }
-
-  subjectSearch() {
+  subjectSearchSolo() {
     for (let i = 0; i < this.courseInfo.length; i++) {
-      if ((this.courseInfo[i].subject === this.subject_entry && this.courseInfo[i].catalog_nbr === this.component_entry)) {
+      if ((this.courseInfo[i].subject === this.subject_entry.toUpperCase())) {
 
         this.array_1.push({
           "course_info": this.courseInfo[i].course_info
@@ -32,6 +33,19 @@ export class FrontendComponent implements OnInit {
         console.log(this.array_1)
       }
 
+    }
+    if (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(this.subject_entry)) { alert('ERROR: You have entered illegal characters.') }
+  }
+
+  subjectSearch() {
+    for (let i = 0; i < this.courseInfo.length; i++) {
+      if ((this.courseInfo[i].subject === this.subject_entry.toUpperCase() && this.courseInfo[i].catalog_nbr === this.component_entry.toUpperCase())) {
+
+        this.array_1.push({
+          "course_info": this.courseInfo[i].course_info
+        });
+        console.log(this.array_1)
+      }
 
     }
     if (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(this.subject_entry) || (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(this.component_entry))) { alert('ERROR: You have entered illegal characters.') }
